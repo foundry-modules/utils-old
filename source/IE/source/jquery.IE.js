8084d6071abe5097e84efacc674ba57e4ea6ddfe
Jason Ramos <jason@stackideas.com>
2013-10-17 14:52:41 +0800

    Added support for IE10 for $.IE.

--- source/IE/source/jquery.IE.js
+++ source/IE/source/jquery.IE.js
@@ -1,12 +1,14 @@
 $.IE = (function(){
 
+    if (navigator.appVersion.indexOf("MSIE 10") != -1) return 10;
+    
     var undef,
         v = 3,
         div = document.createElement('div'),
         all = div.getElementsByTagName('i');
 
     while (
-    	v++,
+        v++,
         div.innerHTML = '<!--[if gt IE ' + v + ']><i></i><![endif]-->',
         all[0]
     );
