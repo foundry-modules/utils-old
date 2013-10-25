6b61b102220190e766c8f0d13d28c18c820aec87
Jason Ramos <jason@stackideas.com>
2013-10-25 12:38:04 +0800

    Update sanitizeHTML to work with newer jQuery.

--- source/sanitizeHTML/source/jquery.sanitizeHTML.js
+++ source/sanitizeHTML/source/jquery.sanitizeHTML.js
@@ -1,11 +1,3 @@
 $.sanitizeHTML = function(html) {
-
-	var fragmentContainer = document.createElement('div'),
-	fragment = document.createDocumentFragment();
-
-	$.clean([html], document, fragment);
-
-	fragmentContainer.appendChild(fragment);
-
-	return fragmentContainer.innerHTML;
+	return $($.parseHTML(html)).toHTML();
 };
