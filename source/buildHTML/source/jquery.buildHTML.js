58b95f68dce6ea04587f309c677e8847d61d59aa
Jason Ramos <jason@stackideas.com>
2013-11-08 14:35:21 +0800

    Prevent blank textnodes, accept jquery element and insert script within last html node when possible.

--- source/buildHTML/source/jquery.buildHTML.js
+++ source/buildHTML/source/jquery.buildHTML.js
@@ -1,19 +1,50 @@
 $.buildHTML = function(html, keepScripts) {
 
-	var callback,
-		scripts = [];
+	// If a jquery element was passed in, convert to html string,
+	// then trim out any whitespace so no unusable text nodes are introduced.
+	var html = $.trim(html instanceof $ ? html.toHTML() : html),
 
-	// If we want to remove the script after it is executed
-	if (!keepScripts) {
+		// Build html fragment while keeping a separate reference to the script
+		scripts = [],
+		fragment = $.buildFragment([html], document, scripts),
 
-		// Create script removal callback
-		callback = document.createElement("script");
-		callback.text = $.callback(function(){$(scripts).remove();}) + "();";
+		// Convert childNodes into a proper array
+		nodes = $.merge([], fragment.childNodes);
+
+	// If we want to remove the script after
+	// it is appended to the DOM & executed
+	if (!keepScripts && scripts.length > 0) {
+
+		// Create script remover
+		var script = document.createElement("script");
+			script.text = $.callback(function(){$(scripts).remove();}) + "();";
+
+		// Go through nodes in reverse
+		var i = nodes.length-1, node, inserted;
+
+		while (node = nodes[i--]) {
+
+			// If a script node is found first, we'll just append
+			// script remover next to it to ensure this last script
+			// executes before any script removal happens.
+			if (node.nodeName==="SCRIPT") {
+				inserted = nodes.push(script);
+			} else if (node.nodeType===1) {
+				inserted = node.appendChild(script);
+			}
+
+			if (inserted) break;
+		}
+
+		// If script remover was not inserted,
+		// then just add it to the array of nodes
+		if (!inserted) nodes.push(script);
+
+		// Add script remover itself to the
+		// array of scripts to be removed.
+		scripts.push(script);
 	}
 
-	// Build html fragment while keeping a separate reference to the script
-	var fragment = $.buildFragment([html, callback], document, scripts);
-
-	// Convert nodes of the fragment into jquery instance
-	return $($.merge([], fragment.childNodes));
+	// Convert nodes into jquery instance and return
+	return $(nodes);
 };
